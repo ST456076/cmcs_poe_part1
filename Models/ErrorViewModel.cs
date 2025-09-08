33bba02060a1025aafa3f23c61093a35c4f4718f
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-09-08 05:39:32 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cmcs_poe_part1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
